1a0a751facb3e6526cd2bcb7dfaddd2b142df4f4
AJ Clarke <aj@Alexanders-iMac-2.local>
2015-09-17 13:18:00 -0700

    Added missing archive/404 header

--- index.php
+++ index.php
@@ -17,6 +17,23 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		<?php if ( is_archive() ) : ?>
+
+			<header class="page-header clr">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+		<?php elseif ( is_404() ) : ?>
+
+			<header class="page-header clr">
+				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'wpex' ); ?></h1>
+			</header><!-- .page-header -->
+
+		<?php endif; ?>
+
 		<?php if ( have_posts() ) : ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
